f4e9a48fd1a9f1d9c43e2f3802394a1271db6ebf
IliykhaDobrinskiy <bogdobrinskiy@gmail.com>
2021-11-10 16:08:01 +0300

    CREATED MainFrame

---
+++ src/MainPackege/MainFrame.java
@@ -0,0 +1,12 @@
+package MainPackege;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    MainFrame(){
+        setSize(600,600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+    }
+}
